7399a498fa0c01ac49ad8431fb1ea042ecd19153
Victorino Machava <victorino.machava@vm.co.mz>
2023-12-01 01:08:20 +0200

    chore: extend tailwind theme with colors from design system

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,29 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        orange: "#D87D4A",
+        "orange-light": "#FBAF85",
+        black: "#101010",
+        dark: "#000",
+        grey: "#F1F1F1",
+        greyish: "#FAFAFA",
+        white: "#FFF",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
