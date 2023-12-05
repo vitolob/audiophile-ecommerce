f9bafb901baaf9eaf8e08a40becae8865cea95fd
Victorino Machava <victorino.machava@vm.co.mz>
2023-12-05 17:10:38 +0200

    fix(arrowbutton): improve component styling

--- src/components/atoms/ArrowButton.tsx
+++ src/components/atoms/ArrowButton.tsx
@@ -6,14 +6,14 @@ const ArrowButton: FC<ButtonHTMLAttributes<HTMLButtonElement>> = ({
   className,
   ...rest
 }) => {
+  const baseClassList =
+    "flex items-center tracking-[1px] text-xs font-bold uppercase";
+
   return (
-    <button
-      {...rest}
-      className="flex items-center tracking-[1px] text-xs font-bold uppercase"
-    >
+    <button {...rest} className={`${baseClassList} ${className || ""}`.trim()}>
       <span className="opacity-50 hover:opacity-100 hover:text-orange">
         {children}
-      </span>{" "}
+      </span>
       <ArrowIcon className="ml-2" />
     </button>
   );
