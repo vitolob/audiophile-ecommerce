658ca4f01238bec74948da967fcf9304f737a1ea
Victorino Machava <victorino.machava@vm.co.mz>
2023-12-06 01:16:30 +0200

    chore(tests): update jest config to enable coverage collection

--- jest.config.mjs
+++ jest.config.mjs
@@ -14,6 +14,7 @@ const config = {
 
   testEnvironment: "jest-environment-jsdom",
   setupFilesAfterEnv: ["<rootDir>/tests/testSetup.ts"],
+  collectCoverage: true,
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
